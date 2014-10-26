ec635a6cab658106c9b23b06a2146601021ea323
flyingsleeves <flyingsleeves@gmail.com>
2014-10-26 17:47:36 -0500

    fix some conflicts

--- app/src/main/java/org/gamecontrol/codeclock/HomeActivity.java
+++ app/src/main/java/org/gamecontrol/codeclock/HomeActivity.java
@@ -17,7 +17,7 @@ import java.util.Arrays;
 import java.util.UUID;
 
 
-public class Home extends Activity {
+public class HomeActivity extends Activity {
 
     private static final String TAG_FILE = "Files";
 
@@ -44,7 +44,7 @@ public class Home extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
+        // automatically handle clicks on the HomeActivity/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
