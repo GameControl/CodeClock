e910cfa06f4665a7a7d0be2f12c9452bf43146ec
iAmRoot <cmontague09@gmail.com>
2014-10-26 23:26:17 -0500

    Timer now updates text while running

--- app/src/main/java/org/gamecontrol/codeclock/JobActivity.java
+++ app/src/main/java/org/gamecontrol/codeclock/JobActivity.java
@@ -11,16 +11,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.os.Handler;
 import android.widget.Button;
 
+import java.util.Timer;
 import java.util.UUID;
+import java.util.TimerTask;
 
 
-public class JobActivity extends Activity {
+public class JobActivity extends Activity{
     private String parentProject = "";
     private TimeService.TimeContainer timeContainer;
+    private Button jobTimer;
     private Job currentJob;
+    private Handler handler;
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,8 @@ public class JobActivity extends Activity {
         parentProject = intent.getStringExtra(ProjectActivity.EXTRA_PROJECT_NAME);
 
         //TODO
+        jobTimer = (Button) findViewById(R.id.jobTimerButton);
+        handler = new Handler();
         currentJob = createJob();
 /*        if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
@@ -46,6 +53,16 @@ public class JobActivity extends Activity {
 
         ActionBar actionBar = getActionBar();
         actionBar.setTitle(parentProject + " > " + currentJob.getName());
+        if(timeContainer == null)
+            timeContainer = TimeService.TimeContainer.getInstance();
+
+        // If TimeContainer holds data for a different job, save that progress, and load the current job
+        if(timeContainer.getJobUUID() != currentJob.getUUID()) {
+            timeContainer.saveJob();
+            timeContainer.loadJob(currentJob);
+        }
+        startUpdateTimer();
+        updateTimeText();
     }
 
 
@@ -70,14 +87,6 @@ public class JobActivity extends Activity {
 
     public void toggleTimer(View view) {
         startService();
-        if(timeContainer == null)
-            timeContainer = TimeService.TimeContainer.getInstance();
-
-        // TimeContainer holds data for a different job
-        if(timeContainer.getJobUUID() != currentJob.getUUID()) {
-            timeContainer.saveJob();
-            timeContainer.loadJob(currentJob);
-        }
         if(timeContainer.getCurrentState() == TimeService.TimeContainer.STATE_RUNNING){
             //change ring to pause
             Button timer = (Button) findViewById(R.id.jobTimerButton);
@@ -113,6 +122,72 @@ public class JobActivity extends Activity {
 
     public void markComplete(View view){
 
+    }
+
+    private final Runnable updateTextRunnable = new Runnable() {
+        @Override
+        public void run() {
+            updateTimeText();
+        }
+    };
+
+    private void updateTimeText() {
+        jobTimer.setText(getTimeString(timeContainer.getTotalElapsed()));
+    }
+
+    public void startUpdateTimer() {
+        if(timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(updateTextRunnable);
+            }
+        }, 0, 16);
+    }
+
+    public String getTimeString(long ms) {
+        if(ms == 0) {
+            return "00:00";
+        } else {
+            long seconds = (ms / 1000) % 60;
+            long minutes = (ms / 1000) / 60;
+            long hours = minutes / 60;
+
+            StringBuilder sb = new StringBuilder();
+            if(hours > 0) {
+                sb.append(hours);
+                sb.append(':');
+            }
+            if(minutes > 0) {
+                minutes = minutes % 60;
+                if(minutes >= 10) {
+                    sb.append(minutes);
+                } else {
+                    sb.append(0);
+                    sb.append(minutes);
+                }
+            } else {
+                sb.append('0');
+                sb.append('0');
+            }
+            sb.append(':');
+            if(seconds > 0) {
+                if(seconds >= 10) {
+                    sb.append(seconds);
+                } else {
+                    sb.append(0);
+                    sb.append(seconds);
+                }
+            } else {
+                sb.append('0');
+                sb.append('0');
+            }
+            return sb.toString();
+        }
     }
 
     /**
