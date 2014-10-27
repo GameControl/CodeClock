fa9e9388b6a04bf33327d7cfc287a4ce5921509f
flyingsleeves <flyingsleeves@gmail.com>
2014-10-27 18:00:34 -0500

    preventing empty and duplicate project names, creating project file upon success

--- app/src/main/java/org/gamecontrol/codeclock/HomeActivity.java
+++ app/src/main/java/org/gamecontrol/codeclock/HomeActivity.java
@@ -15,20 +15,16 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.UUID;
 
 
 public class HomeActivity extends Activity {
@@ -175,53 +171,6 @@ public class HomeActivity extends Activity {
             View rootView = inflater.inflate(R.layout.fragment_home, container, false);
             return rootView;
         }
-    }
-
-    public void testProjectSave() {
-        // Make dummy projects and jobs
-        Project testProject = makeDummyProject();
-
-        CodeClockJSONSerializer testSerializer = new CodeClockJSONSerializer(this);
-        try {
-            testSerializer.saveProject(testProject, testProject.getUuid().toString() + ".json");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public Project makeDummyProject() {
-        Project dummyProject = new Project();
-
-        dummyProject.setUuid(UUID.randomUUID());
-
-        ArrayList<Job> jobs = new ArrayList<Job>();
-        jobs.add(makeDummyJob(dummyProject.getUuid()));
-        jobs.add(makeDummyJob(dummyProject.getUuid()));
-        jobs.add(makeDummyJob(dummyProject.getUuid()));
-        dummyProject.setJobs(jobs);
-
-        ArrayList<String> tags = new ArrayList<String>(Arrays.asList("Peace", "has", "sapped", "your", "strength."));
-        dummyProject.setTags(tags);
-
-        dummyProject.setNotes("Victory has defeated you.");
-
-
-
-        return dummyProject;
-    }
-
-    public Job makeDummyJob(UUID projectUUID) {
-        Job dummyJob = new Job(UUID.randomUUID(), projectUUID, "", 1, null);
-
-        dummyJob.setName("JobName" + Math.round(Math.random()*10));
-        dummyJob.setEstimate(Math.round(Math.random() * 100));
-        dummyJob.setNotes("Enter notes here...");
-        ArrayList<String> tags = new ArrayList<String>(Arrays.asList("your", "spirit", "or", "your body!"));
-        dummyJob.setTags(tags);
-
-        return dummyJob;
     }
 }
 
